e55f9857fc166880f8eba6e7dabf279b9777c80e
Ard-Jan van Etten <ardjanve@gmail.com>
2021-02-12 13:58:44 +0100

    Revert "E"
    This reverts commit 9031cb449b0fee7c0894b493258ff3adbeef178f.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { Worker, isMainThread, parentPort, threadId } = require('worker_threads');
-
+const stationAfstandKilonet = require('./functies/stationAfstandKilonet.js');
 const leesJSONSync = require('./functies/leesJSONSync.js');
 
 const {
-    tijdNaarMinutenGetal
+    tijdNaarMinutenGetal,
+    minutenGetalNaarTijd
 } = require('./functies/utility.js');
+
+const {
+    stationVertrekkenMoment
+} = require('./functies/interpreters.js');
 
 const config = leesJSONSync("config");
 const vertrekken = leesJSONSync('vertrekken');
@@ -13,11 +17,79 @@ const stations = leesJSONSync('stations');
 const startTijdMinuten = tijdNaarMinutenGetal(config.starttijd);
 const eindTijdMinuten = startTijdMinuten - - config.speelduur_minuten;
 
+const stationsNaam = (stationsCode) => stations.find((kandidaat) => stationsCode == kandidaat[1])[9];
+
 let meesteAfstand = 0;
 
-let workers = [];
-let lopendeThreads = 0;
-let wachtrij = [[
+const berekenRitjes = (aankomstTijdMinuten, station, negeerbareFeaturesReferentie, huidigeAfstand, routeTotNuToe, routeDeltas, nietVolgen) => {
+    const vroegsteVertrektijd = aankomstTijdMinuten + config.minimum_overstaptijd_minuten;
+
+    // check of rit tot nu toe nog voldoet
+    if (
+        vroegsteVertrektijd > eindTijdMinuten
+        || (
+            routeDeltas.length > config.snelheidsmetingen_begin_ritjes &&
+            huidigeAfstand / ((aankomstTijdMinuten - startTijdMinuten) / 60) < config.minimum_gemiddelde_snelheid
+        ) || (
+            routeDeltas.length > config.maximum_ritjes_stilstand &&
+            routeDeltas.slice(- config.maximum_ritjes_stilstand - 1).reduce((a, b) => a + b) == 0
+        ) || !vertrekken[station]
+    ) return;
+
+    const laatsteVertrekTijd = aankomstTijdMinuten + config.maximum_overstaptijd_minuten;
+    let negeerbareFeatures = [...negeerbareFeaturesReferentie];
+
+    if (huidigeAfstand > meesteAfstand) {
+        meesteAfstand = huidigeAfstand;
+        const routeString = "afstand: " + (Math.round(huidigeAfstand * 10) / 10) + " km\n" + routeTotNuToe.map((deel, index) => [
+            minutenGetalNaarTijd,
+            stationsNaam,
+            minutenGetalNaarTijd
+        ][index % 3](deel)).join("\n") + "\n";
+        
+        if (meesteAfstand >= config.minimale_update_afstand) {
+            console.log(routeString);
+        }
+    }
+
+    // sort?
+    let ritjes = stationVertrekkenMoment(station, vroegsteVertrektijd, laatsteVertrekTijd);
+    let berekendeVertrekken = [];
+
+    for (const rit of ritjes) {
+        if (rit.richting == nietVolgen) continue;
+        if (berekendeVertrekken.includes(rit.richting)) continue;
+        berekendeVertrekken.push(rit.richting);
+
+        let afstand = huidigeAfstand;
+        
+        const vertrekTijd = rit.verdererit[0].vertrektijd;
+
+        let vorigeStation = station;
+        for (const vertrek of rit.verdererit.slice(1)) {
+            afstand += stationAfstandKilonet(vorigeStation, vertrek.station, negeerbareFeatures);
+
+            // er wordt op het station gestopt
+            if (vertrek.stopt) {
+                berekenRitjes(
+                    vertrek.aankomsttijd,
+                    vertrek.station,
+                    negeerbareFeatures,
+                    afstand,
+                    [...routeTotNuToe, vertrekTijd, vertrek.aankomsttijd, vertrek.station],
+                    [...routeDeltas, afstand - huidigeAfstand],
+                    rit.richting
+                );
+            }
+
+            vorigeStation = vertrek.station;
+        }
+    }
+};
+console.log(stationVertrekkenMoment('lw', 600, 660))
+console.log("==========BEGIN==========");
+
+berekenRitjes(
     startTijdMinuten,
     config.start_station,
     [],
@@ -25,48 +97,4 @@ let wachtrij = [[
     [startTijdMinuten, config.start_station],
     [],
     ''
-]];
-
-const probeerOpdracht = () => {
-    if (lopendeThreads >= config.threads) return;
-    if (wachtrij.length == 0) return;
-
-    const worker = new Worker("./worker.js");
-
-    worker.onmessage = (data) => {
-        if (data.type = 'nieuwbest') {
-            meesteAfstand = data.inhoud;
-        } else if (data.type == 'opdracht') {
-            wachtrij.push(data.inhoud);
-            probeerOpdracht();
-        } else if (data.type == 'klaar') {
-            lopendeThreads--;
-            worker.terminate();
-            delete worker;
-        }
-    };
-
-    worker.postMessage({
-        type: 'initialisatie',
-        inhoud: {
-            config,
-            vertrekken,
-            stations,
-            startTijdMinuten,
-            eindTijdMinuten
-        }
-    });
-
-    const opdracht = wachtrij[wachtrij.length - 1];
-    wachtrij.pop();
-
-    worker.postMessage({
-        type: 'opdracht',
-        inhoud: opdracht
-    })
-
-    workers.push(worker);
-}
-
-console.log("==========BEGIN==========");
-probeerOpdracht();
+);
